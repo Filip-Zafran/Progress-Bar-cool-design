575c42a5a8117b400676f57105d0427f0646dc4c
FichoZee <filip.zafran@gmail.com>
2020-03-25 14:54:29 +0100

    divided files

---
+++ moi.js
@@ -0,0 +1,29 @@
+function fillBar(seconds) {
+  const bar = document.querySelector(".bar");
+
+  let atPercent = 0;
+
+  const interval = setInterval(() => {
+    bar.style.width = atPercent + "%";
+    atPercent++;
+
+    // check if its running correctly
+    console.log("running at", atPercent);
+
+    // check that the percentages doesnt go over 100
+    if (atPercent >= 100) {
+      clearInterval(interval);
+    }
+
+    // set that it updates 1% every millisecond
+  }, (seconds * 1000) / 100);
+}
+
+// heart
+var bar = new ProgressBar.Path("#heart-path", {
+  easing: "easeInOut",
+  duration: 1400
+});
+
+bar.set(0);
+bar.animate(1.0);
